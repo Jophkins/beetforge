21e5968e2f70697d1788a72a50b45dad8abb3e42
Jophkins <sergebrew@gmail.com>
2026-02-02 17:10:45 -0700

    :hammer: fix: fixed prisma 7 adapter connection

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,20 @@
 import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 import { env } from "prisma/config";
 
 import { PrismaClient } from "../generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  adapter: new PrismaPg({ connectionString: env("DATABASE_URL") }),
-  log: ["warn", "error"],
-});
+const connectionString = env("DATABASE_URL");
+const adapter = new PrismaPg({ connectionString });
+
+export const prisma
+  = globalForPrisma.prisma
+    ?? new PrismaClient({
+      adapter,
+      log: ["warn", "error"],
+    });
 
 if (env("NODE_ENV") !== "production")
   globalForPrisma.prisma = prisma;
